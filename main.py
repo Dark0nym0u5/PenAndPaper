65d9078af1faf3d7552614366df73f6820b71de8
Dark0nym0u5 <pentester.ils@gmail.com>
2020-12-26 19:48:08 +0200

    test

---
+++ main.py
@@ -0,0 +1,36 @@
+# 1. Information Gathering Protocol - Reconnaissance
+# Network
+# Host
+# People involved
+# Google Hacking
+
+
+# 2. Scanning The Target To Find My Way In - Scanning
+# Port scanning
+# NMAP
+
+# Vulnerability Scanning
+# WPSCAN
+# Nessus
+
+# Network Mapping
+# Maltego
+
+# 3. Gaining Access To My Target - Gaining Access
+# privilege escalation
+# Metasploit
+
+# 4. Make Sure You Maintaining Access - Maintaining Access
+# Rootkits
+# keyloggers
+# Trojans
+
+# 5. Clean The Remains - Clearing Track
+# System Logs
+# Apache Access File
+# Apache Error File
+
+
+
+
+
